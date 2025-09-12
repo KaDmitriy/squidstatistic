5b3e99d56846210711c8f417a2d915de38523c4d
KaDmitriy <ka.dmitriy@gmail.com>
2025-09-12 18:35:07 +0300

    change sql db

---
+++ sql/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.content_type ("id", "name") VALUES (0, '-');
+
+INSERT INTO public."domain" ("id", "name") VALUES	 (0, '-');
+
+INSERT INTO public.hierarchy_code ("id", "name") VALUES	 (0, '-');
+
+INSERT INTO public.request_method ("id", "name") VALUES (0, '-');
+
+INSERT INTO public.result_code ("id", "name") VALUES (0, '-');
